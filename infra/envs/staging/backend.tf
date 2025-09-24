b9e746cfb4ce67e0d5864ef03fe56716551f5f28
lyleplaatjes <90036644+lyleplaatjes@users.noreply.github.com>
2025-09-24 21:56:14 +0200

    bucket created and updated

--- infra/envs/staging/backend.tf
+++ infra/envs/staging/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "bonmoja-terraform-state"
+    bucket         = "bonmoja-terraform-state-670278274130"
     key            = "staging/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks"
