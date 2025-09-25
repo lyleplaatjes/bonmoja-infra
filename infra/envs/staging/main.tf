2dc66a44a5cacfc2b97c8710c8f4f2d267abdb5f
lyleplaatjes <90036644+lyleplaatjes@users.noreply.github.com>
2025-09-25 13:06:37 +0200

    Added missing brace

--- infra/envs/staging/main.tf
+++ infra/envs/staging/main.tf
@@ -4,13 +4,14 @@ terraform {
         aws = {
             source  = "hashicorp/aws"
             version = "~> 5.0"
+        }
         random = {
             source  = "hashicorp/random"
             version = "~> 3.0"
-         }
         }
     }
 }
+
 
 provider "aws" {
     region = var.aws_region
