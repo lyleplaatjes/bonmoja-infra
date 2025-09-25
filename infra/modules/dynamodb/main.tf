a2a50219ca38dfbff1b712d1810e65b8d67ef28c
lyleplaatjes <90036644+lyleplaatjes@users.noreply.github.com>
2025-09-25 13:10:56 +0200

    fixing block formatting

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -1,8 +1,11 @@
 resource "aws_dynamodb_table" "this" {
   name         = var.name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "PK"
-  attribute { name = "PK"; type = "S" }
+  hash_key            = "PK"
+  attribute { 
+    name = "PK"
+    type = "S" 
+    }
 }
 output "table_arn" { value = aws_dynamodb_table.this.arn }
 output "table_name" { value = aws_dynamodb_table.this.name }
